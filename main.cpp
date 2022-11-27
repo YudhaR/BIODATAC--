49d1f4e37fc382590d975b05e75f94a7cd397e07
YudhaR <yudharizqia182@gmail.com>
2022-11-28 04:23:46 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "BIODATA" << endl;
+    cout << "Nama : Yudha Rizqia Grafer" << endl;
+    cout << "NIM  : A11.2022.14280" << endl;
+    cout << "TTL  : Tegal 6 Juli 2004" << endl;
+    cout << "Hobi : Rebahan" << endl;
+    return 0;
+}
